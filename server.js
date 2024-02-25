cdb3ad0f39fbc5bcd8d538e863673549f082512c
Ulad <vladis67948@gmail.com>
2024-02-25 14:31:20 +0300

    cors: added cors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
-import { authRoutes } from './app/auth/auth.routes.js'
+import express from 'express'
 import morgan from 'morgan'
-import { prisma } from './app/prisma.js'
+import path from 'path'
+
 import { errorHandler, notFound } from './app/middleware/error.middleware.js'
-import { usersRoutes } from './app/user/user.routes.js'
+
+import { authRoutes } from './app/auth/auth.routes.js'
 import { API_BASE, API_ROUTES } from './app/constants/api.constants.js'
 import { exercisesRoutes } from './app/exercise/exercise.routes.js'
-import path from 'path'
+import { prisma } from './app/prisma.js'
+import { usersRoutes } from './app/user/user.routes.js'
 import { workoutRoutes } from './app/workout/workout.routes.js'
 
 dotenv.config()
@@ -31,6 +34,8 @@ async function main() {
 	app.use(express.json())
 
 	const __dirname = path.resolve()
+
+	app.use(cors())
 
 	app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
